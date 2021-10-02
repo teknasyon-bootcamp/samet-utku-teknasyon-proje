0dda768170827635f3805df46e8a4aa4e72bbab1
Samet Utku Olgun <smtkuo@gmail.com>
2021-10-02 03:09:41 +0300

    Router and Controller Added

--- Api/members/Vendor/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php 
-spl_autoload_register(function($class){
-	$path = str_replace("\\","/",$class);
-	$fileRoad = (__DIR__)'/../' . $path . '.class.php';
-	if(!file_exists($fileRoad)){
-		return false;
-	} 
-    include_once $fileRoad;
-}); 
