d202f1441f8ef9de7cb54bc3978c66e383a5e540
Samet Utku Olgun <smtkuo@gmail.com>
2021-10-13 23:51:10 +0300

    index update

--- www/public/index.php
+++ www/public/index.php
@@ -19,39 +19,35 @@ set_exception_handler('\Core\error::exceptionHandler');
  * Routing
 */
 $router = new \Core\router;
+/* Home */
 $router->add("GET",["url"=>"/","controller"=>"index","action"=>"index","dynamic"=>1]);
-// DynamicPages
+/* DynamicPages */
 $router->add("POST",["url"=>"/","controller"=>"index","action"=>"pageViewer","dynamic"=>1]);
 $router->add("POST",["url"=>"/news","controller"=>"news_Controller","action"=>"index","dynamic"=>1]);
 $router->add("POST",["url"=>"/category","controller"=>"news_Controller","action"=>"category","dynamic"=>1]);
-/* Membership */
+/* Membership Signup or Signin */
 $router->add("POST",["url"=>"/api/members/signup","controller"=>"signup_Controller","action"=>"Signup"]);
 $router->add("POST",["url"=>"/api/members/signin","controller"=>"signin_Controller","action"=>"Signin"]);
-/* Comments */
+/* Comment Add */
 $router->add("POST",["url"=>"/api/comments/add","controller"=>"comments_Controller","action"=>"add"]);
 /* Panel */
 $router->add("POST",["url"=>"/panel","controller"=>"panel_Controller","action"=>"index","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["user"]]); } ]);
 $router->add("POST",["url"=>"/logout","controller"=>"panel_Controller","action"=>"logout","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["user"]]); } ]);
-/* News Index */
+/* Panel Editor */
 $router->add("POST",["url"=>"/panel/news","controller"=>"panel_Controller","action"=>"editor","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["editor"]]); } ]);
-/* Categories */
+/* Category Editor */
 $router->add("POST",["url"=>"/panel/news/categoryAdd","controller"=>"panel_Controller","action"=>"categoryAdd","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["editor"]]); } ]);
 $router->add("POST",["url"=>"/panel/news/categoryDelete","controller"=>"panel_Controller","action"=>"categoryDelete","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["editor"]]); } ]);
-/* News */
+/* News Editor */
 $router->add("POST",["url"=>"/panel/news/newsAdd","controller"=>"panel_Controller","action"=>"newsAdd","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["editor"]]); } ]);
 $router->add("POST",["url"=>"/panel/news/newsDelete","controller"=>"panel_Controller","action"=>"newsDelete","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["editor"]]); } ]);
-/* Moderator or Admin */
+/* User Editor */
 $router->add("POST",["url"=>"/panel/users","controller"=>"panel_Controller","action"=>"moderator_admin","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["moderator"]]); } ]);
 $router->add("POST",["url"=>"/panel/user/update","controller"=>"panel_Controller","action"=>"userUpdate","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["moderator"]]); } ]);
 $router->add("POST",["url"=>"/panel/user/delete","controller"=>"panel_Controller","action"=>"userDelete","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["moderator"]]); } ]);
-
-
-
-/* Recent Activities */
+/* Last Activities */
 $router->add("POST",["url"=>"/panel/latest","controller"=>"panel_Controller","action"=>"latest","middleware"=>function(){ return (new \Core\middleware\auth)->rule(["permissions"=>["user"]]); } ]);
-
 /* Router Test */
 $router->add("GET",["url"=>"/test","callback"=>function(){ echo 'Teknasyon.Com GetContact Panteon.games '; }]);
-
 /* CallBack */
 $router->run();
